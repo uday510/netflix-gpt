06683d58109def633247378ac2a5d3cc669a1b2e
Uday <budayteja009@gmail.com>
2024-02-27 21:13:48 +0530

    fix secondary container css

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import VideoTitle from './VideoTitle';
-import VideoBackground from './VideoBackground';
-import { MOVIE_POSTER_URL } from '../utils/constants';
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
-  const movies = useSelector(store => store.movies?.nowPlayingMovies);
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+
   if (!movies) return;
 
-  const mainMovie = movies[9];
+  const mainMovie = movies[0];
 
   const { original_title, overview, id } = mainMovie;
 
-  // console.log(movies);
   return (
-    <div>
-      {/* {mainMovie && <img src={`${MOVIE_POSTER_URL}${mainMovie.backdrop_path}`} alt={mainMovie.title} />} */}
+    <div className="pt-[30%] bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview} />
       <VideoBackground movieId={id} />
     </div>
   );
-}
-
-export default MainContainer
+};
+export default MainContainer;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IMG_CDN_URL } from '../utils/constants';
+
+const MovieCard = ({ posterPath }) => {
+  return (
+    <div className='w-36 md:w-48 pr-4'>
+      <img
+        alt="Movie Card"
+        src={IMG_CDN_URL + posterPath}>
+      </img>
+    </div>
+  );
+}
+
+export default MovieCard;
