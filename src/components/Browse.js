a6356f9f71481dd9ae77f6de3f28084d179fc619
Uday <budayteja009@gmail.com>
2024-02-27 15:43:40 +0530

    Add vide title and play button

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,29 +1,22 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies } from '../utils/movieSlice';
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 
 const Browse = () => {
-  const dispatch = useDispatch();
 
-  const getNowPlayingMovies = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results))
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  // Fetch Now Playing Movies
+  useNowPlayingMovies();
 
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
-  )
+  );
+  
 }
 
 export default Browse
---
+++ src/components/MainContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import VideoTitle from './VideoTitle';
+import VideoBackground from './VideoBackground';
+import { MOVIE_POSTER_URL } from '../utils/constants';
+import { over } from 'lodash';
+
+const MainContainer = () => {
+  const movies = useSelector(store => store.movies?.nowPlayingMovies);
+  if (!movies) return;
+
+  const mainMovie = movies[0];
+
+  const { original_title, overview } = mainMovie;
+
+  return (
+    <div>
+      {/* {mainMovie && <img src={`${MOVIE_POSTER_URL}${mainMovie.backdrop_path}`} alt={mainMovie.title} />} */}
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground />
+    </div>
+  );
+
+}
+
+export default MainContainer
